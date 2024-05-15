ee76c3a93e222b8173f87f1c0c10299522d2ebe0
TEDiks2003 <maciej.kwasnik@gmail.com>
2024-05-15 19:23:06 +0100

    Spline Interpolations

--- InterpolationFunc.py
+++ InterpolationFunc.py
@@ -90,5 +90,69 @@ def piecewise_linear_spline(x_arr: np_farr, y_arr: np_farr) -> list[np_polynomia
     return ret
 
 
-def cubic_spline(x_arr: np_farr, y_arr: np_farr) -> list[np_polynomial]:
-    pass
+def cubic_spline_manual(x_arr: np_farr, y_arr: np_farr, s_d_0: float, s_d_n: float, clamped: bool = True) -> list[np_polynomial]:
+    """s_d_0 = S'(x_0), s_d_n = S'(x_N), ONLY WORKS FOR 4 DATA POINTS"""
+    n = len(x_arr)
+    h_k = [x_arr[i+1]-x_arr[i] for i in range(n-1)]
+    d_k = [(y_arr[i+1]-y_arr[i])/h_k[i] for i in range(n-1)]
+    u_k = [6*(d_k[i]-d_k[i-1]) for i in range(1, n-1)]
+
+    assert n == 4, "We have a problem"
+
+    if clamped:
+        c_1 = 2*(h_k[0]+h_k[1])-(h_k[0]/2)
+        c_2 = 2*(h_k[n-3]+h_k[n-2])-(h_k[n-2]/2)
+        e_1 = -3*(d_k[0] - s_d_0)
+        e_2 = -3*(s_d_n-d_k[n-2])
+    else:
+        c_1 = 2*(h_k[0]+h_k[1])
+        c_2 = 2*(h_k[n-3]+h_k[n-2])
+        e_1 = 0
+        e_2 = 0
+
+    row_1 = [c_1, h_k[1]]
+    row_2 = [h_k[2], c_2]
+    mat = np.array([row_1, row_2], dtype=np.float64)
+    b = np.asfarray([u_k[0]+e_1, u_k[1]+e_2])
+
+    sol = np.linalg.solve(mat, b)
+
+    if clamped:
+        m_0 = (3/h_k[0])*(d_k[0]-s_d_0)-(sol[0]/2)
+        m_3 = (3/h_k[n-2])*(d_k[n-2])-(sol[1]/2)
+    else:
+        m_0 = 0
+        m_3 = 0
+
+    m_k = [m_0, sol[0], sol[1], m_3]
+
+    s_k_1 = y_arr
+    s_k_2 = [d_k[i]-(h_k[i]*(2*m_k[i]+m_k[i+1]))/6 for i in range(n-1)]
+    s_k_3 = [m_k[i]/2 for i in range(n-1)]
+    s_k_4 = [(m_k[i+1]-m_k[i])/(6*h_k[i]) for i in range(n-1)]
+
+    for i in range(n-1):
+        print(f"s_{i}(x) = {s_k_4[i]}(x-{x_arr[i]})^3+{s_k_3[i]}(x-{x_arr[i]})^2+{s_k_2[i]}(x-{x_arr[i]})+{s_k_1[i]}")
+
+    g_k_1 = [np.poly1d([1, -x]) for x in x_arr]
+    g_k_2 = [np.polymul(g, g) for g in g_k_1]
+    g_k_3 = [np.polymul(g, h) for g, h in zip(g_k_1, g_k_2)]
+    ret = []
+    for i in range(n-1):
+        a_0 = np.poly1d([s_k_1[i]])
+        a_1 = np.poly1d([s_k_2[i]])
+        a_2 = np.poly1d([s_k_3[i]])
+        a_3 = np.poly1d([s_k_4[i]])
+        b = np.polymul(a_1, g_k_1[i])
+        c = np.polymul(a_2, g_k_3[i])
+        d = np.polymul(a_3, g_k_3[i])
+        f_1 = np.polyadd(a_0, b)
+        f_2 = np.polyadd(c, d)
+        ret.append(np.polyadd(f_1, f_2))
+
+    return ret
+
+
+def cubic_spline_scipy(x_arr: np_farr, y_arr: np_farr, ends: ((float, float), (float, float))):
+    return sc.interpolate.CubicSpline(x_arr, y_arr, bc_type=ends)
+
--- main.py
+++ main.py
@@ -8,62 +8,10 @@ from random import sample
 
 
 def program():
-    mat_str = """
-    0,2,1,0,0,0,2,0,0,0,28717;
-    0,1,0,2,0,0,1,0,0,0,2612;
-    0,3,2,0,0,1,0,0,0,0,2371;
-    0,0,3,0,1,0,1,0,0,0,11957;
-    0,1,0,3,1,0,0,0,0,0,8368;
-    3,0,0,0,0,0,1,1,0,0,9978;
-    2,1,0,0,0,0,0,0,1,0,9203;
-    0,2,1,1,0,0,0,0,0,0,11275;
-    0,0,0,0,1,0,0,1,0,0,9257;
-    0,5,0,1,0,0,0,0,0,0,30226;
-    0,1,2,0,1,0,0,0,1,0,4885;
-    1,0,0,0,0,0,1,0,1,0,22566;
-    0,1,1,2,0,0,0,1,0,0,2468;
-    0,0,2,0,1,0,0,0,1,0,11513;
-    2,2,2,0,0,1,0,0,0,0,23705;
-    5,0,2,0,0,0,0,1,0,0,11874;
-    2,1,1,0,1,0,0,0,1,0,32110;
-    0,0,0,1,0,1,2,0,0,0,7255;
-    2,1,0,1,0,1,1,0,0,0,1245;
-    1,0,2,2,0,0,0,0,0,0,3783;
-    1,3,1,1,0,0,0,0,0,0,12000;
-    0,1,0,0,0,0,1,1,0,0,7607;
-    0,3,2,0,0,0,0,0,0,1,8637;
-    """
-    mat = Matrix(mat_str)
-    arr = list(mat.get_arr.tolist())
-    print([len(row) for row in arr])
-    while True:
-        sub_arr = list(sample(arr, 10))
 
-        b = []
-        for i in range(len(sub_arr)):
-            b.append(sub_arr[i].pop())
-
-        print(sub_arr)
-        mat_arr = np.array(sub_arr)
-
-        mat = Matrix(mat_arr)
-        if mat.get_determinant != 0:
-            print(len(mat.get_arr))
-            break
-
-        for i in range(len(sub_arr)):
-            sub_arr[i].append(b[i])
-    print(mat)
-    print(b)
-
-    mat.append_b(np.asfarray(b))
-    sol = mat.solve_mod_p(17)
-    print(sol)
-    print(mat)
-
-    x = [0, 5, 37, sol[1]]
-    n = [4, 9, 43, 17]
-    print(crt(n, x))
+    x = np.asfarray([0, 1, 2, 3])
+    y = np.asfarray([0, 0.5, 2.0, 1.5])
+    cubic_spline_manual(x, y, 0.2, -1, False)
 
 
 
